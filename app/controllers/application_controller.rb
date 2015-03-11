320dce2c2986a3a354928fbc5e5ed2e48f28010c
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2015-03-11 18:03:58 +0300

    integrate devise & active admin; fix turbolinks issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def authenticate_admin_user!
+    if current_admin_user
+      true
+    else
+      flash[:alert] = "Admin user required!"
+      redirect_to new_user_session_path
+    end
+  end
+
+  def current_admin_user
+    current_user && current_user.admin? && current_user
+  end
 end
