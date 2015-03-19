71ed02113ca462c7990141887a71256c3ebc5635
Maksym Bodnarchuk <bodnarchuk@gmail.com>
2015-03-19 13:54:35 +0200

    support lazy translation

--- lib/model_translations.rb
+++ lib/model_translations.rb
@@ -3,21 +3,37 @@ module ModelTranslations
 
   class_methods do
     def translate(*attributes)
-      attributes.each do |attr|
-        ["#{attr}_ru", "#{attr}_en"].each do |localized_attr|
-          unless self.columns_hash.key? localized_attr
-            raise ArgumentError, "Unknown attribute #{localized_attr.inspect} for class #{name}"
-          end
-        end
+      @@translate_attributes = attributes
+    end
 
-        define_method attr do
-          send("#{attr}_#{I18n.locale}")
-        end
+    def translate_attributes
+      @@translate_attributes
+    end
+  end
 
-        define_method "#{attr}=" do |new_value|
-          raise RuntimeError, "Do not use #{self.class.name}##{attr}=, use ##{attr}_en= or ##{attr}_ru= instead"
+  def create_method(name, &block)
+    self.class.send(:define_method, name, &block)
+  end
+
+  def method_missing(m, *args, &block)
+    attr = m.to_sym
+    if self.class.translate_attributes.include? attr
+      ["#{attr}_ru", "#{attr}_en"].each do |localized_attr|
+        unless self.class.columns_hash.key? localized_attr
+          raise ArgumentError, "Unknown attribute #{localized_attr.inspect} for class #{self.class.name}"
         end
       end
+
+      create_method attr do
+        send("#{attr}_#{I18n.locale}")
+      end
+
+      create_method "#{attr}=" do |new_value|
+        raise RuntimeError, "Do not use #{self.class.name}##{attr}=, use ##{attr}_en= or ##{attr}_ru= instead"
+      end
+      send(attr)
+    else
+      super
     end
   end
 end
