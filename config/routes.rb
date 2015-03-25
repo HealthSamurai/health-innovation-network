430647257a3202bf4ec22fb8ecb7d21ccd55f051
Danil Kutkevich <danil@kutkevich.org>
2015-03-25 03:45:10 +0300

    Example of pages without routes and actions.
    Example howto create pages without routes and actions.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   resources :stories
 
-  get '/pages/membership', to: 'pages#membership' 
+  get '/pages/:action', to: 'pages'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
