06b1ea724bf278c696b738ff78a795579eb3d4d0
niquola <niquola@gmail.com>
2015-04-30 17:09:57 +0400

    fixes #19 admin users

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,6 +1,29 @@
 ActiveAdmin.register User do
   scope :all, default: true
-  permit_params :email
+  permit_params :email,
+    :encrypted_password,
+    :admin,
+    :sign_in_count,
+    :unconfirmed_email,
+    :name,
+    :position,
+    :position_is_public,
+    :city,
+    :state,
+    :phone,
+    :company_name,
+    :company_position,
+    :company_category_id,
+    :company_is_accelerator,
+    :company_ace_name,
+    :company_ace_url,
+    :company_ace_contact,
+    :linkedin,
+    :areas_willing_to,
+    :engagement_interest_other,
+    :interest_area_other,
+    :is_public,
+    :interest_number_one_issue
 
   index do
     selectable_column
