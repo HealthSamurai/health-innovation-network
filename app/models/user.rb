afef018d7c4e12304ca29e1744e9428621d38a23
Danil Kutkevich <danil@kutkevich.org>
2015-04-30 19:11:07 +0300

    FIX: #14 paperclip default for user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar,
     :styles => { :medium => "300x300>", :thumb => "100x100>" },
-    :default_url => ActionController::Base.helpers.asset_path("avatar.png")
+    default_url: 'avatar--:style.png'
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
